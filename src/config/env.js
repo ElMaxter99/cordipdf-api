ba474d677746516fd298a5791ca8c8afe69f174d
Alvaro Amorós <39102625+ElMaxter99@users.noreply.github.com>
2025-11-28 08:38:37 +0100

    Fix Mongo docker defaults

--- src/config/env.js
+++ src/config/env.js
@@ -4,14 +4,14 @@ const numberFromEnv = (value, defaultValue) => {
 };
 
 const mongoHost = process.env.MONGO_HOST || 'mongo';
-const mongoPort = process.env.MONGO_PORT || 13023;
-const mongoDb = process.env.MONGO_DB || 'fuel';
+const mongoPort = process.env.MONGO_PORT || 23023;
+const mongoDb = process.env.MONGO_DB || 'cordipdf-bda';
 
 const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: numberFromEnv(process.env.PORT, 3000),
   MONGO_HOST: mongoHost,
-  MONGO_PORT: numberFromEnv(mongoPort, 13023),
+  MONGO_PORT: numberFromEnv(mongoPort, 23023),
   MONGO_DB: mongoDb,
   MONGO_URI: process.env.MONGO_URI || `mongodb://${mongoHost}:${mongoPort}/${mongoDb}`,
   MONGO_EXPRESS_PORT: numberFromEnv(process.env.MONGO_EXPRESS_PORT, 8081),
